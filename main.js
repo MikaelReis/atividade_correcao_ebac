37ab41e46df5c3619c9d80e4cbd7f58d77d2263a
MikaelReis <mikaelreis08@gmail.com>
2025-01-07 18:02:16 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('#carousel-imagens').slick({
+        autoplay: true
+    });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, digite seu nome',
+            email: 'Por favor, digite seu e-mail',
+            telefone: 'Por favor, digite seu telefone',
+            cpf: 'Por favor, digite seu CPF',
+            cep: 'Por favor, digite seu CEP'
+        },
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+});
