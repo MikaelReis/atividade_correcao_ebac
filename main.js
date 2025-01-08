822edd0927e4abb184b251b4a77327973f0feffb
MikaelReis <mikaelreis08@gmail.com>
2025-01-08 15:07:21 -0300

    Atualiza index.html e main.js com as novas alterações

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ $(document).ready(function(){
         placeholder: '(00) 00000-0000'
     });
 
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '00000-000'
+    });
+
     $('form').validate({
         rules: {
             nome: {
